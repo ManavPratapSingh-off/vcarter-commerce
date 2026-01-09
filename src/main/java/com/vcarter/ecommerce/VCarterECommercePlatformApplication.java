87edc90ebb58474377e0fc6400a7319f1af6e852
ManavPratapSingh-off <manav3407.gb@gmail.com>
2026-01-09 17:53:02 +0530

    Initial commit

---
+++ src/main/java/com/vcarter/ecommerce/VCarterECommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.vcarter.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VCarterECommercePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VCarterECommercePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcarter/ecommerce/VCarterECommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcarter.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VCarterECommercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
